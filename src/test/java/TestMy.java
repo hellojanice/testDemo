bdb7c18711c576db9ebc73d8fadeb2ebf8c9a3a7
janice.zhang <janice.zhang@ringcentral.com>
2017-09-05 13:25:23 +0800

    commit

--- src/test/java/TestMy.java
+++ src/test/java/TestMy.java
@@ -1,6 +1,10 @@
-import org.junit.Test;
+//import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+//import static junit.framework.TestCase.assertEquals;
 
 
 /**
@@ -13,7 +17,7 @@ public class TestMy {
         MyClass tester = new MyClass(); // MyClass is tested
 
         // assert statements
-        assertEquals("0 add 10 must be 10", 10, tester.addMethod(0, 10));
-//        assertEquals("0 add 10 must not be 0", 0, tester.addMethod(0, 10));
+//        assertEquals("0 add 10 must be 10", 10, tester.addMethod(0, 10));
+        assertEquals("0 add 10 must not be 0", 0, tester.addMethod(0, 10));
     }
 }
