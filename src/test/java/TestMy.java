423116404bca6d14aa4f5aefbb36c836b5e7d4d9
janice.zhang <janice.zhang@ringcentral.com>
2017-09-05 21:05:49 +0800

    commit

--- src/test/java/TestMy.java
+++ src/test/java/TestMy.java
@@ -17,7 +17,13 @@ public class TestMy {
         MyClass tester = new MyClass(); // MyClass is tested
 
         // assert statements
-//        assertEquals("0 add 10 must be 10", 10, tester.addMethod(0, 10));
         assertEquals("0 add 10 must not be 0", 0, tester.addMethod(0, 10));
+    }
+    @Test
+    public void testMyClassPass() {
+        MyClass tester = new MyClass(); // MyClass is tested
+
+        // assert statements
+        assertEquals("0 add 10 must be 10", 10, tester.addMethod(0, 10));
     }
 }
