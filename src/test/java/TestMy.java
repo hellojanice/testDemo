36ec064ab34da815bd10d42bd547e5b01ec7aa53
janice.zhang <janice.zhang@ringcentral.com>
2017-09-20 13:25:07 +0800

    commit

--- src/test/java/TestMy.java
+++ src/test/java/TestMy.java
@@ -11,19 +11,10 @@ import static org.testng.AssertJUnit.assertEquals;
  * Created by janice.zhang on 9/5/17.
  */
 public class TestMy {
-
-    @Test
-    public void testMyClass() {
-        MyClass tester = new MyClass(); // MyClass is tested
-
-        // assert statements
-        assertEquals("0 add 10 must not be 0", 0, tester.addMethod(0, 10));
-    }
     @Test
     public void testMyClassPass() {
-        MyClass tester = new MyClass(); // MyClass is tested
-
-        // assert statements
-        assertEquals("0 add 10 must be 10", 10, tester.addMethod(0, 10));
+        MyClass tester = new MyClass();
+         int expectValue=Integer.valueOf(System.getProperty("expectValue"));
+        assertEquals("0 add 5 must be 5", expectValue, tester.addMethod(0, 5));
     }
 }
